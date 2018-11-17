6ce750ebc0b28d1bec57249cfc59721127feb1d3
Karina Golovin <karina@Mac-mini-Andrey.local>
2018-11-17 20:48:14 +0200

    Displayed name and time for each player

--- js/stat.js
+++ js/stat.js
@@ -3,12 +3,14 @@
 
 (function () {
   window.renderStatistics = function (ctx, names, times) {
-    var gap = 50;
+    var gapX = 50;
+    var gapY = 10;
     var columnWidth = 40;
     var maxColumnHeight = 150;
     var baseX = 155;
     var baseY = 90;
     var maxTime = getMaxValue(times);
+    var me = 'Вы';
 
     ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
     ctx.fillRect(110, 20, 420, 270);
@@ -16,8 +18,6 @@
     ctx.strokeRect(99, 9, 422, 272);
     ctx.fillStyle = 'white';
     ctx.fillRect(100, 10, 420, 270);
-    ctx.fillStyle = 'blue';
-    ctx.strokeRect(155, 90, 310, 150);
     ctx.fillStyle = 'black';
     ctx.font = '16px "PT Mono"';
     ctx.fillText('Ура вы победили!', baseX, 40, [360]);
@@ -25,20 +25,26 @@
 
     for (var i = 0; i < names.length; i++) {
       var randomSaturationNumber = Math.floor(Math.random() * 100);
-      var playerTime = times[i];
+      var playerTime = Math.round(times[i]);
+      var playerName = names[i];
 
       var ratio = playerTime / maxTime;
       var playerColumnHeight = maxColumnHeight * ratio;
 
       var playerColumnY = baseY + maxColumnHeight - playerColumnHeight;
 
-      if (names[i] === 'Вы') {
+      ctx.fontStyle = 'black';
+      ctx.textBaseline = 'middle';
+      ctx.fillText(playerTime, baseX + (gapX + columnWidth) * i, playerColumnY - gapY, [50]);
+      ctx.fillText(playerName, baseX + (gapX + columnWidth) * i, baseY + maxColumnHeight + gapY);
+
+      if (names[i] === me) {
         ctx.fillStyle = 'rgba(255, 0, 0, 1)';
       } else {
         ctx.fillStyle = 'hsl(240, 100%, ' + randomSaturationNumber + '%)';
       }
 
-      ctx.fillRect(baseX + (gap + columnWidth) * i, playerColumnY, columnWidth, playerColumnHeight);
+      ctx.fillRect(baseX + (gapX + columnWidth) * i, playerColumnY, columnWidth, playerColumnHeight);
     }
   };
 
