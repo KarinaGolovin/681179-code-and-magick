c71b434a2536d23366da316c7d523a7ca2873bf2
Karina Golovin <karina@Mac-mini-Andrey.local>
2018-11-22 20:50:07 +0200

    Vrapped code with IIFE

--- js/setup.js
+++ js/setup.js
@@ -1,96 +1,99 @@
 'use strict';
 
-var characterPopupPanel = document.querySelector('.setup');
-var similarCharacterBlock = characterPopupPanel.querySelector('.setup-similar');
-// place to put template list
-var randomCharacterList = similarCharacterBlock.querySelector('.setup-similar-list');
-// wizard template to clone >>> randomCharacterList
-var similarCharacterTemplate = document.querySelector('#similar-wizard-template');
+(function () {
+  var characterPopupPanel = document.querySelector('.setup');
+  var similarCharacterBlock = characterPopupPanel.querySelector('.setup-similar');
+  // place to put template list
+  var randomCharacterList = similarCharacterBlock.querySelector('.setup-similar-list');
+  // wizard template to clone >>> randomCharacterList
+  var similarCharacterTemplate = document.querySelector('#similar-wizard-template');
 
-characterPopupPanel.classList.remove('hidden');
-similarCharacterBlock.classList.remove('hidden');
+  characterPopupPanel.classList.remove('hidden');
+  similarCharacterBlock.classList.remove('hidden');
 
-var characterCount = 4;
-var wizardNames = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон',
-];
+  var characterCount = 4;
+  var wizardNames = [
+    'Иван',
+    'Хуан Себастьян',
+    'Мария',
+    'Кристоф',
+    'Виктор',
+    'Юлия',
+    'Люпита',
+    'Вашингтон',
+  ];
 
-var wizardSurnames = [
-  'да Марья',
-  'Верон',
-  'Мирабелла',
-  'Вальц',
-  'Онопко',
-  'Топольницкая',
-  'Нионго',
-  'Ирвинг',
-];
+  var wizardSurnames = [
+    'да Марья',
+    'Верон',
+    'Мирабелла',
+    'Вальц',
+    'Онопко',
+    'Топольницкая',
+    'Нионго',
+    'Ирвинг',
+  ];
 
-var coatColors = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)',
-];
+  var coatColors = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)',
+  ];
 
-var eyesColors = [
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'green',
-];
+  var eyesColors = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green',
+  ];
 
-// get random number in the range from...to
-var getRandomNumber = function (minNumber, maxNumber) {
-  return Math.floor(minNumber + (Math.random() * (maxNumber + 1 - minNumber)));
-};
-
-// generate random character options
-var createCharacter = function () {
-  var randomCharacter = {
-    name: wizardNames[getRandomNumber(0, wizardNames.length - 1)],
-    surname: wizardSurnames[getRandomNumber(0, wizardSurnames.length - 1)],
-    coatColor: coatColors[getRandomNumber(0, coatColors.length - 1)],
-    eyesColor: eyesColors[getRandomNumber(0, eyesColors.length - 1)],
-    fullname: function () {
-      return randomCharacter.name + ' ' + randomCharacter.surname;
-    }
+  // get random number in the range from...to
+  var getRandomNumber = function (minNumber, maxNumber) {
+    return Math.floor(minNumber + (Math.random() * (maxNumber + 1 - minNumber)));
   };
-  return randomCharacter;
-};
 
-// render random character template
-var renderRandomCharacter = function (character) {
-  // clone template
-  var characterElement = similarCharacterTemplate.content.cloneNode(true);
-  // link on template wizard options
-  var characterNameTemplate = characterElement.querySelector('.setup-similar-label');
-  var characterCoatTemplate = characterElement.querySelector('.wizard-coat');
-  var characterEyesTemplate = characterElement.querySelector('.wizard-eyes');
-  // rewrite template options on random character options
-  characterNameTemplate.textContent = character.fullname();
-  characterCoatTemplate.style.fill = character.coatColor;
-  characterEyesTemplate.style.fill = character.eyesColor;
+  // generate random character options
+  var createCharacter = function () {
+    var randomCharacter = {
+      name: wizardNames[getRandomNumber(0, wizardNames.length - 1)],
+      surname: wizardSurnames[getRandomNumber(0, wizardSurnames.length - 1)],
+      coatColor: coatColors[getRandomNumber(0, coatColors.length - 1)],
+      eyesColor: eyesColors[getRandomNumber(0, eyesColors.length - 1)],
+      fullname: function () {
+        return randomCharacter.name + ' ' + randomCharacter.surname;
+      }
+    };
+    return randomCharacter;
+  };
 
-  return characterElement;
-};
+  // render random character template
+  var renderRandomCharacter = function (character) {
+    // clone template
+    var characterElement = similarCharacterTemplate.content.cloneNode(true);
+    // link on template wizard options
+    var characterNameTemplate = characterElement.querySelector('.setup-similar-label');
+    var characterCoatTemplate = characterElement.querySelector('.wizard-coat');
+    var characterEyesTemplate = characterElement.querySelector('.wizard-eyes');
+    // rewrite template options on random character options
+    characterNameTemplate.textContent = character.fullname();
+    characterCoatTemplate.style.fill = character.coatColor;
+    characterEyesTemplate.style.fill = character.eyesColor;
 
-// create fragment to hold all characters before append to randomCharacterList
-var fragment = document.createDocumentFragment();
-// clone characters and send them to randomCharacterList
-for (var i = 1; i <= characterCount; i++) {
-  var randomCharacter = createCharacter();
-  var renderTemplate = renderRandomCharacter(randomCharacter);
-  fragment.appendChild(renderTemplate);
-}
-randomCharacterList.appendChild(fragment);
+    return characterElement;
+  };
+
+  // create fragment to hold all characters before append to randomCharacterList
+  var fragment = document.createDocumentFragment();
+  // clone characters and send them to randomCharacterList
+  for (var i = 1; i <= characterCount; i++) {
+    var randomCharacter = createCharacter();
+    var renderTemplate = renderRandomCharacter(randomCharacter);
+    fragment.appendChild(renderTemplate);
+  }
+  randomCharacterList.appendChild(fragment);
+})();
+
